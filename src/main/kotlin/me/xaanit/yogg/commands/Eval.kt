170d45de3702cf62badc9db87c6ff67b902d93ce
xaanit <shadowjacob@yahoo.com>
2019-05-02 17:05:17 -0700

    Switch to desktop

--- src/main/kotlin/me/xaanit/yogg/commands/Eval.kt
+++ src/main/kotlin/me/xaanit/yogg/commands/Eval.kt
@@ -14,8 +14,8 @@ import javax.script.SimpleBindings
 
 class Eval : Command(name = "eval", admin = true) {
     override suspend fun execute(member: Member, channel: TextChannel, guild: Guild, message: Message, db: Postgres) {
-        val engine = ScriptEngineManager().getEngineByName("kts")
-
+        val engine = ScriptEngineManager().getEngineByName("kotlin")
+        val start = System.currentTimeMillis()
         val res = Either.from {
             engine.evalKotlin(message.content()!!.substring("!eval ".length), SimpleBindings(hashMapOf<String, Any>(
                     "member" to member,
@@ -25,28 +25,36 @@ class Eval : Command(name = "eval", admin = true) {
                     "db" to db
             )))
         }
+        val result = res.toString().max(size = 2040)
+
         channel.newEmbed {
             it.setColor(when (res) {
                 is Right -> Color(0xdd021b)
                 else -> Color(0x249999)
             })
             it.setAuthor("Eval", null, HeroPictures.getUrl(""))
+            it.setFooter("${System.currentTimeMillis() - start}ms", null)
             it.setTimestamp(Instant.now())
-            it.setDescription(
-                    res.toString().max(size = 2048)
-            )
+            if (result.matches("(?:([^:/?#]+):)?(?:\\//([^/?#]*))?([^?#]*\\.(?:jpg|gif|png))(?:\\?([^#]*))?(?:#(.*))?".toRegex())) it.setImage(result) else it.setDescription("""
+               ```
+               $result
+               ```
+           """.trimIndent())
         }
     }
 
-    fun ScriptEngine.evalKotlin(script: String, bindings: Bindings): Any? {
+    fun ScriptEngine.evalKotlin(originalScript: String, bindings: Bindings): Any? {
         val injection =
-                bindings.entries.joinToString("\n") { (key, value) ->
-                    "val $key" +
-                            ": ${value::class.java.simpleName} = bindings[\"$key\"] as ${value::class.java.name}"
-                }
-        println("INJECTION: $injection")
-        return eval("$injection\n$script", bindings)
+                bindings.keys.joinToString("\n") { key -> "val $key = bindings[\"$key\"] as ${key::class.java.name}" }
+        val lastImportIndex = originalScript.lastIndexOf("import ")
+        val endOfImports = originalScript.substring(lastImportIndex).indexOf("\n") + lastImportIndex
+        val imports = originalScript.substring(0, endOfImports)
+        val script = originalScript.substring(endOfImports)
+        val evalScript = "$imports\n$injection\n$script"
+        println(evalScript)
+        return eval(evalScript, bindings)
     }
+
 
     private fun String.max(size: Int): String = substring(0, Math.min(length, size))
 }
